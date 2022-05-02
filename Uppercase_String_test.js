b8ee2ce4ec222f3d3e0b3ebd0abb1c51b52d58d3
Harold Doger <35712424+HaroldDoger@users.noreply.github.com>
2022-05-02 10:37:35 -0400

    Add files via upload

---
+++ Uppercase_String_test.js
@@ -0,0 +1,14 @@
+// Is the string uppercase?
+// Task
+// Add the isUpperCase method to String to see whether the string is ALL CAPS. For example:
+
+// "c".isUpperCase() == false
+// "C".isUpperCase() == true
+// "hello I AM DONALD".isUpperCase() == false
+// "HELLO I AM DONALD".isUpperCase() == true
+// "ACSKLDFJSgSKLDFJSKLDFJ".isUpperCase() == false
+// "ACSKLDFJSGSKLDFJSKLDFJ".isUpperCase() == true
+
+String.prototype.isUpperCase = function() {
+    return this.toUpperCase() === this.toString();
+  }
