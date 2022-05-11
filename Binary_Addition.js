83053d50b78747e13d23b92158cb319eab339d5d
Harold Doger <35712424+HaroldDoger@users.noreply.github.com>
2022-05-11 18:29:50 -0400

    Add files via upload

---
+++ Binary_Addition.js
@@ -0,0 +1,6 @@
+function addBinary(a,b){
+    return (a+b).toString(2) //toString(2) is a to string method that auto converts an integer to its binary value in string format
+  }
+
+  //shorthand solution
+  const addBinary = (a, b) => (a + b).toString(2);
