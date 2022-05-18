541dba9e01def66d05301f13db64a7bc9449e325
Harold Doger <35712424+HaroldDoger@users.noreply.github.com>
2022-05-17 23:58:40 -0400

    Update Categorize_New_Member.js

--- Categorize_New_Member.js
+++ Categorize_New_Member.js
@@ -1,1 +1,6 @@
-
+function openOrSenior(data){
+  function determineMembership(member){
+    return (member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open';
+  }
+  return data.map(determineMembership);
+}
