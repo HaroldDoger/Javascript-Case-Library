d427228c7105b31a48423e6aac1a64d4ba5829c6
Harold Doger <35712424+HaroldDoger@users.noreply.github.com>
2022-05-24 12:45:21 -0400

    Create is_This_A_Triangle.js

---
+++ is_This_A_Triangle.js
@@ -0,0 +1,22 @@
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+//solution
+function isTriangle(a,b,c)
+{
+  if (a === 0 || b === 0 || c === 0){ //if any value is a 0, it automatically can not be true
+    return false;
+  }
+  else if (a + c <= b){ //no side of a triangle can exceed the sum of the other two sides
+   return false;
+  }
+  
+  else if ( b + c <= a){
+    return false;
+    }
+  else if (a + b <= c){
+    return false;
+  }
+  return true; //if all conditions are met, return true
+}
