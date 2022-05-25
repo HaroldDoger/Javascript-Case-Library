5d4efb16676d7e50f6a641ce4b0ff6d2796c4afd
Harold Doger <35712424+HaroldDoger@users.noreply.github.com>
2022-05-25 18:22:55 -0400

    Create Square_root_test.js

---
+++ Square_root_test.js
@@ -0,0 +1,37 @@
+// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+
+//my solution
+
+var isSquare = function(n){
+  if ( n < 0){ //perfect squares can not be negative therefore if n is < 0 return false no matter what
+    return false;
+  }
+  
+  if (Number.isInteger(Math.sqrt(n))){ //number.isinteger checks if the result is a whole number
+    return true;                       //math.sqrt(n) will square root the input n
+  } 
+  else{
+    return false}
+}
+
+//best practice solution
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
