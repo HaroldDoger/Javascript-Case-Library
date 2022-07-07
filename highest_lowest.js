f506590c2543a0a4bbcd0644d2df4a5043a36420
harold doger <harolddoger@gmail.com>
2022-07-07 00:30:32 -0400

    best practice solution revision

--- highest_lowest.js
+++ highest_lowest.js
@@ -19,3 +19,8 @@ function highAndLow(numbers) {
 
     return maxNum + " " + minNum      //return results of max and min
 }
+
+//best practice / clever solution
+// function highAndLow(numbers){
+//     numbers = numbers.split(' ').map(Number);
+//     return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
