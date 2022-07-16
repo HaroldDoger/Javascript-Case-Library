31aa1d8e709a2e08a2524932278b19686f9d0e08
harold doger <harolddoger@gmail.com>
2022-07-16 15:36:12 -0400

    file upload

---
+++ remove_anchor.js
@@ -0,0 +1,9 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+    // TODO: complete
+  }
