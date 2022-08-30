98c37ed76cc0ffff125e48cc7ebf92aa8c904e26
harold doger <harolddoger@gmail.com>
2022-08-29 20:01:26 -0400

    upload

--- exclamationMark1.js
+++ exclamationMark1.js
@@ -1,1 +1,4 @@
 // Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+function remove (string) {
+    return string.replace(/!$/,'')
+   }
