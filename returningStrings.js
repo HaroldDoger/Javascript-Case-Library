1cf756372d989529d6f9c6fcf22793b8d789a514
Harold Doger <35712424+HaroldDoger@users.noreply.github.com>
2022-09-01 13:19:25 -0400

    Create returningStrings.js

---
+++ returningStrings.js
@@ -0,0 +1,15 @@
+// Make a function that will return a greeting statement
+// that uses an input; your program should return,
+//   "Hello, <name> how are you doing today?".
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+//to use template literals the use of backticks `` must be used 
+//the traditional strings indicators of "" or '' 
+//will NOT work for template literals
+
+//single line solution
+// const greet = name => `Hello, ${name} how are you doing today?`;
+
+//encapsulate variables with ${expression}
