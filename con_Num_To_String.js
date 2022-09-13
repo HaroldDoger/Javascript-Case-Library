8d3f1fed1c9055f4ba868c94f7031fa8a5945a71
harold doger <harolddoger@gmail.com>
2022-09-13 14:38:58 -0400

    default code

--- con_Num_To_String.js
+++ con_Num_To_String.js
@@ -2,7 +2,7 @@ function numberToString(num) {
     return num.toString();// Return a string of the number here!
   }
 
-  //best practice solution 23
+  //best practice solution 234
 //   function numberToString(num) {
 //     // Return a string of the number here!
 //     return String(num);
