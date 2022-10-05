47b6589fa3c8bf172712f9bd9419e9d3a4fa3ded
harold doger <harolddoger@gmail.com>
2022-10-05 18:53:22 -0400

    solution upload

--- array_Mean.js
+++ array_Mean.js
@@ -7,3 +7,7 @@
       
         return Math.floor(arr.reduce((a,c)=> a + c)/ arr.length)
     }
+
+    console.log(getAverage([2,2,2,2]),2)
+console.log(getAverage([1,2,3,4,5,]),3)
+console.log(getAverage([1,1,1,1,1,1,1,2]),1)
