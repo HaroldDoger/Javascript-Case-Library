d0ffc545f69e79a5f8ee4d84c03926d36fa2ac55
harold doger <harolddoger@gmail.com>
2022-10-08 16:25:14 -0400

    file done

--- transportation_Vacation.js
+++ transportation_Vacation.js
@@ -5,3 +5,18 @@
 // Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 
 // Write a code that gives out the total amount for different days(d).
+
+function rentalCarCost(d) {
+    let total = 0
+    
+    if(d < 3){
+      return total = d * 40
+  
+    }
+    else if (d < 7 && d >= 3){
+      return  total = (d * 40) - 20
+      }
+    else {
+      return  total = (d * 40) - 50
+    }
+  }
