01abdce4214d91bd85c127f36871933d7c81b5ca
harold doger <harolddoger@gmail.com>
2022-10-15 17:22:55 -0400

    answer

--- find_the_odd_int.js
+++ find_the_odd_int.js
@@ -8,3 +8,15 @@
 // [1,1,2] should return 2, because it occurs 1 time (which is odd).
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+
+//my answer
+function findOdd(arr) {
+    for(let i = 0; i < arr.length; i++){
+      const count = arr.filter(value => value === arr[i]).length;
+        if(count % 2 == 1){
+          return arr[i];
+        }
+    }
+    
+  }
