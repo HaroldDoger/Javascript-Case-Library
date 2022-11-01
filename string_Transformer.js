47e1df65b32aaaa6ebf14c9a742769dd9389d05d
harold doger <harolddoger@gmail.com>
2022-10-31 21:27:20 -0400

    file upload

--- string_Transformer.js
+++ string_Transformer.js
@@ -8,3 +8,25 @@
 
 // "Example Input" ==> "iNPUT eXAMPLE"
 // You may assume the input only contain English alphabet and spaces.
+function stringTransformer(str) {
+    var charArr = str.split(' ').reverse().join(' ').split('')
+    
+    for(let i = 0; i < charArr.length; i++){
+      if(charArr[i] === charArr[i].toUpperCase()){
+        charArr[i] = charArr[i].toLowerCase();
+      }
+      else{
+        charArr[i] = charArr[i].toUpperCase();
+      }
+      
+    }
+    var fullString = charArr.join("").split(' ').join(' ')
+   return fullString
+  }
+  
+  //one line solution
+  // function stringTransformer(str) {
+  //  let transformStr = str => str.split(' ').reverse().join(' ').split('')
+  //  .map(l => l === l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()).join('')
+  //  return console.log(transformStr);
+  // }
