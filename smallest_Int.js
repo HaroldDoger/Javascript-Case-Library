d07acc2cab2bed585708cb3f7c1337a70c167aca
harold doger <harolddoger@gmail.com>
2022-11-13 16:10:55 -0500

    file upload

---
+++ smallest_Int.js
@@ -0,0 +1,28 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+  args.sort(function(a, b){return a-b});
+      return args[0]
+    }
+  }
+
+  //arrow function
+  class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return args.sort((a,b)=>a-b)[0];
+    }
+  }
+
+  //best practice solution
+  class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+    }
+  }
