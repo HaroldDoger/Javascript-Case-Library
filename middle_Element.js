25f57786bee7b1502578839541621ab71fc79c59
Harold Doger <35712424+HaroldDoger@users.noreply.github.com>
2022-11-14 18:38:24 -0500

    Create middle_Element.js

---
+++ middle_Element.js
@@ -0,0 +1,21 @@
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+function gimme (arr) {
+return arr.indexOf([...arr].sort((a,b) => a - b)[1])
+}
+
+//most popular answer
+function gimme(a) {
+  return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+}
