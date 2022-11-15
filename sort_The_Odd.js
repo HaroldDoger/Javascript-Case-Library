518bcf4f35c06ca40f9517fd3e2d72b14b91cf19
harold doger <harolddoger@gmail.com>
2022-11-15 18:31:47 -0500

    file changes

--- sort_The_Odd.js
+++ sort_The_Odd.js
@@ -13,3 +13,34 @@ function sortArray(array) {
     
     return array.map(n => n%2 ===0 ? n : oddNums.pop())
   }
+
+  //other solution
+  function sortArray(array) {
+    const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+    return array.map((x) => x % 2 ? odd.shift() : x);
+  }
+
+  //brute force solution
+
+  function sortArray(array) {
+    var odds = [];
+    //loop, if it's odd, push to odds array
+    for (var i = 0; i < array.length; ++i) {
+      if (array[i]%2 !== 0) {
+        odds.push(array[i]);
+      }
+    }
+    //sort odds from smallest to largest
+    odds.sort(function(a,b){
+      return a-b;
+    });
+    
+    //loop through array, replace any odd values with sorted odd values
+    for (var j = 0; j < array.length; ++j) {
+      if (array[j]%2 !== 0) {
+        array[j] = odds.shift();
+      }
+    }
+    
+   return array;
+  }
