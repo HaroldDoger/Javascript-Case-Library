c60a66af05ae9e08ca1f3651f09ee446dfe00adb
harold doger <harolddoger@gmail.com>
2023-02-08 17:32:32 -0500

    upload

--- km_to_s.js
+++ km_to_s.js
@@ -5,9 +5,33 @@
 // For example:
 
 // 1.08 --> 30
-// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer. shad
+// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
 
 function cockroachSpeed(s) {
     return Math.floor(s * 27.778) //multiply kmh by 27.778 to get cm/s, use Math.floor to round down to integer
     
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //s
